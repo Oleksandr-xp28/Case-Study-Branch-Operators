5697563bf4310ca6354ff94eb086822e3c87778a
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-30 20:19:40 +0300

    task 2

---
+++ Case_Study_Branch_Operators.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    try:
+        i = int(input("Enter a number: "))
+        if i % 7 == 0:
+            print("Number is multiple 7")
+        elif i % 7 != 0:
+            print("Number is not multiple 7")
+        pass
+    except:
+        print("Invalid input")
